68bf4e4b96531c7d4ed4f9918035702289078419
woojinjang <greentornado@naver.com>
2021-08-21 00:21:26 +0900

    test

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ def hello_world() -> str:
     print("Hello World!")
     return "Hello"
 
-def bad_function() -> int:
+
+def bad_function(c) -> int:
     a = 1
     b = 2
-    return a + b
+
+    return a + b + c
 
 
 hello_world()
