263fae7564e3d7fdabc47852bca8b72d2bf28fa4
woojinjang <greentornado@naver.com>
2021-08-22 17:08:18 +0900

    change main

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def hello_world() -> str:
     print("Hello World!")
     return "Hello"
 
+#
+# def bad_function(c) -> int:
+#     a = 1
+#     b = 2
+#
+#     return a + b + c
 
-def bad_function(c) -> int:
-    a = 1
-    b = 2
-
-    return a + b + c
-
-
-hello_world()
+if __name__=='__main__':
+    hello_world()
